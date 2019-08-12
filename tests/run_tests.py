96bb8b704de41278d9a725f3d4ce952fcc07a6bf
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-12 12:29:11 +0100

    Added flake8

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,14 +9,10 @@ def run_all_tests():
     result = unittest.TextTestRunner(verbosity=2).run(suite)
     return result.wasSuccessful()
 
-
 def main():
 
     success = run_all_tests()
-    
     sys.exit(0 if success else 1)
-    #if not success:
-        #raise Exception
 
 if __name__ == "__main__":
     main()
