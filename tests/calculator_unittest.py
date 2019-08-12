57365c56675cef93d0ecad3351c216f589afcaa2
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-12 11:45:01 +0100

    restructure and test failure #4

--- tests/calculator_unittest.py
+++ tests/calculator_unittest.py
@@ -1,8 +1,7 @@
 import unittest
-import mock
 
 
-class TestAll(unittest.TestCase):
+class CalculatorUnitTests(unittest.TestCase):
 
    def test_GIVEN_no_test_THEN_return_failure(self):
        self.assertEqual(True, False)
---
+++ tests/run_tests.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+
+#from tests.calculator_unittest import CalculatorUnitTests
+from calculator_unittest import CalculatorUnitTests
+
+def run_all_tests():
+    suite = unittest.TestLoader().discover(".", pattern="*test.py")
+
+    runner = unittest.TextTestRunner()
+    return runner.run(suite)
+
+def main():
+
+    success = run_all_tests()
+    sys.exit(0 if success else 1)
+
+if __name__ == "__main__":
+    main()
