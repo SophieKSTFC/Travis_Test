892b3be3cfe0149ec1f3348a8b8bbea9d4e91042
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-12 12:16:15 +0100

    amended test assertion

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,15 +4,19 @@ import sys
 from calculator_unittest import CalculatorUnitTests
 
 def run_all_tests():
-    suite = unittest.TestLoader().discover(".", pattern="*test.py")
 
-    runner = unittest.TextTestRunner()
-    return runner.run(suite)
+    suite = unittest.TestLoader().discover(".", pattern="*test.py")
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    return result.wasSuccessful():
+
 
 def main():
 
     success = run_all_tests()
+    
     sys.exit(0 if success else 1)
+    #if not success:
+        #raise Exception
 
 if __name__ == "__main__":
     main()
