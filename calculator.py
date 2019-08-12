b29ab06374d02f7c2dbee015be7c9192c74fd374
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-12 15:23:34 +0100

    working methodology, 0 tests

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
 class Calculator():
     def __init__(self):
-        pass
+        self.current_number = 1
+
+    def press_up(self):
+        self.current_number += 1
+
+    def press_down(self):
+        if self.current_number > 0:
+            self.current_number -= 1
+    
+    def set_number(self, number):
+        self.current_number = number
+    
+    def get_number(self):
+        return self.current_number 
