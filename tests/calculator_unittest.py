4d7ff6a3183a9a3a9628d24b524827541e6944af
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-12 16:56:00 +0100

    Started testing

--- tests/calculator_unittest.py
+++ tests/calculator_unittest.py
@@ -1,12 +1,11 @@
 import unittest
+import sys
+sys.path.append('../')
 from calculator import Calculator
-
 
 class CalculatorUnitTests(unittest.TestCase):
 
-    def __init__(self):
-        super(CalculatorUnitTests, self).__init__()
-        self.calc = Calculator()
+    calc = Calculator()
 
     def test_GIVEN_calculater_WHEN_set_number_called_THEN_number_is_correct(self):
         expected_output = 5
