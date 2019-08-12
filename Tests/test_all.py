4dfebd186d85ce87aaae4851351b24a009f1bd80
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2019-08-12 10:40:35 +0100

    Test Failure #3

--- Tests/test_all.py
+++ Tests/test_all.py
@@ -5,5 +5,5 @@ import mock
 class TestAll(unittest.TestCase):
 
    def test_GIVEN_no_test_THEN_return_failure(self):
-       assertEquals(True, False)
+       self.assertEqual(True, False)
        
